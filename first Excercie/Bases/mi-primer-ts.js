6cdea74e825812a7bb8ed00b448178b60ba49da6
ArthurSesma <arturo@deadline.com.mx>
2022-02-21 19:44:01 -0700

    feat: firsts example, add observable mode. and load js from html file

---
+++ first Excercie/Bases/mi-primer-ts.js
@@ -0,0 +1,7 @@
+"use strict";
+const persona = {
+    nombre: 'Arturo',
+    apellido: 'sesma',
+};
+const msg = 'hola mundo';
+console.log(persona.nombre, persona.apellido, msg);
---
+++ first Excercie/Bases/mi-primer-ts.ts
@@ -0,0 +1,8 @@
+const persona = {
+    nombre: 'Arturo',
+    apellido: 'sesma',
+}
+
+const msg: string = 'hola mundo';
+
+console.log(persona.nombre, persona.apellido, msg);
