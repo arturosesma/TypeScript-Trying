c63515e4f5a6cbc0e2fa3c0ada86aaae1d560e05
ArthurSesma <arturo@deadline.com.mx>
2022-02-24 12:34:02 -0700

    feat: add funcion lesson one.

---
+++ first Excercie/Bases/funciones/function.js
@@ -0,0 +1,26 @@
+"use strict";
+(() => {
+    const hero = 'flash';
+    //funcion convensional
+    function returnName() {
+        return hero;
+    }
+    let nombre = returnName();
+    //esta funcion flecha nos ahorramos crear la funcion y mandarla a llamar
+    //directamente la ejecturamos y el retorno la guradamos 
+    //dentro de la variable.
+    //funcion flecha, la variable activateBatisignal 
+    //es un valor de tipo funcion, no de tipo string.
+    const activateBatisignal = () => {
+        return 'batisigna activate';
+    };
+    console.log(typeof activateBatisignal); //function
+    //forma de especificar el tipo de dato que retorna la funcion flecha.
+    const activateBatisignal2 = () => {
+        return 'batisigna activate';
+    };
+    console.log(typeof activateBatisignal2); //string
+    //it's very important put what kind of data return
+    //my funciton because if tomorrow any coder use my function
+    //can know what does function return.
+})();
