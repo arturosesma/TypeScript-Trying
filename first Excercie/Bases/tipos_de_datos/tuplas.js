19f4da59f22a19de4cc310d9617c91628c3a4f6e
ArthurSesma <arturo@deadline.com.mx>
2022-02-23 18:23:47 -0700

    feat: excercises tuplals

---
+++ first Excercie/Bases/tipos_de_datos/tuplas.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    //tupla, trio, quintenta , cuarteta etc...
+    //tuples en ingles, son arreglos limitados en cantidad de elementos
+    // es decir una tupla es una variable de tipo array a la que ya no le puedes meter mas valores, y 
+    //cada item es de un tipo de dato
+    const hero = ['llave', 3];
+    //hero[0] = 50; // marca un error por que numero no es asignable a string
+    hero[0] = 'string';
+    hero[1] = 23445;
+})();
