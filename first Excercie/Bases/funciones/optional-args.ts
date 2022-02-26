bd4675ae95b4d71ff0e1a636f4ed22f83fec7e03
ArthurSesma <arturo@deadline.com.mx>
2022-02-25 18:51:16 -0700

    feat: add optional args

---
+++ first Excercie/Bases/funciones/optional-args.ts
@@ -0,0 +1,20 @@
+(()=>{
+    //argumentos opcionales, puede que un argumento vaya a ser enviado pero puede que no.
+
+    //esto se logra al final del argumento poner un ?  esto significa que puede que si venga o que no
+    const fullName = (firstName:string, lastName?:string) : string => {
+
+        //si la variable no esta definifa entonces toma el valor de las tuberias o pipe.
+        return `${firstName} ${lastName || 'no last name'}`;
+    }
+
+
+    //como podemos ver al colocar los argumentos, no marca error ya que estamos especificando en la guncion
+    //que dependera de si esta o no definida la variable, es decir puedo no enviarla
+    const name = fullName('nombre', 'apellido');
+    const ejemplo2 = fullName('nombre');
+
+    //de manera natural en JS, se puede mandar argumentos opcionales, pero por no tener un tipado de datos
+    //nos podemos meter en un problema enorme al no tener bien definidos los tipos de datos 
+    //y si se puede o no usar la funcion.
+})()
