4ea2925ab2f4b8132bd40db09b6c9053f4b62e4d
ArthurSesma <arturo@deadline.com.mx>
2022-03-03 15:42:57 -0700

    feat: add excersices about objetcts

---
+++ first Excercie/Bases/Objetos/TipandoUnObjeto.js
@@ -0,0 +1,52 @@
+"use strict";
+(() => {
+    //asi indicamos que la variable es de tipo objeto tipo JSON
+    let objeto = {
+        name: "Arturo",
+        age: 24,
+        powers: ['poder 1', 'poder 2']
+    };
+    //definiendo que es objeto y el tipo de priedades que va a tener
+    //en este punto marca error por que age y powers no estan definidos
+    let objeto2 = {
+        name: "Arturo",
+        // age: 24,
+        // powers: ['poder 1', 'poder 2']
+    };
+    //aqui definimos pero decimos que age es de tipo string, marca
+    //error por que le estamos dando un number
+    // let objeto3 : { name:string, age:string, powers: string[] } = {
+    //     name: "Arturo",
+    //     age: 24,
+    //     powers: ['poder 1', 'poder 2']
+    // }
+    //aqui ya esta definido aqui no marca error todo en orden y no marca error
+    let objeto4 = {
+        name: "Arturo",
+        age: 24,
+        powers: ['poder 1', 'poder 2']
+    };
+    //propiedades opcionales denttro de un Objeto tipo JSON, marca error por que no esta definida age
+    //asi marca error por que age no esta definida
+    // let flash: {name:string, age:number, powers: string[]} = {
+    //     name: 'otro name',
+    //     //age:32,
+    //     powers:['strings', '']
+    // }
+    //asi ya no marca error la funcion de arriba por que estamos indicando que es opcional
+    let flash = {
+        name: 'otro name',
+        //age:32,
+        powers: ['strings', '']
+    };
+    flash = {
+        name: 'otro name',
+        age: 32,
+        powers: ['strings', '']
+        //aqui este marca error por que aun no emos agregado un metodo
+        // como propiedad de un objeto.
+        // getNombre(){
+        //     return this.name;
+        // }
+    };
+})();
---
+++ first Excercie/Bases/Objetos/objects.ts
@@ -0,0 +1,37 @@
+(()=>{
+
+    //objetos literales en JS
+
+    //es un objeto literal donde podemos crear un contenedor con diversos valores, 
+    //esto es un objeto.
+
+
+    //si ponemos el cursos encima nos indica el tipo de datos que tienen
+    //este es un objeto de tipo JSON
+    // si ponemos el cursor veremos el tipo de dato
+    let flash = {
+        name: 'Arturo Sesma',
+        age: 21,
+        powers: ['super velocidad', 'viajar en el tiempo']
+    }
+
+    // el objeto debe tener las propiedades que estan definidas arriba
+    //y deben ser los valores del tipo de dato,
+    // no podemos poner otro tipo de dato que no es el que vamos a poner
+    //TS marcara un error de un objeto definido hasta que no este completo.
+    flash = {
+        name: 'otro name',
+        age:32,
+        powers:['strings', '']
+        // getNombre(){
+        //     return this.name;
+        // }
+
+        //que pasa si quiero agregar otra propiedad o quiero hacer
+        //de mis ppropiedades dinamicas
+        //eso se ve en la siguiente leccion.
+    }
+
+    //
+
+})() 
